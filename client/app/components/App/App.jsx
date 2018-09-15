a09fbbf0960796253f4f45f7b434b5241448043a
Julianne Johnson <tweetypie417@yahoo.com>
2018-09-15 18:38:14 -0500

    added react router, basic components

---
+++ client/app/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header.jsx';
+import Main from '../Main/Main.jsx';
+
+
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+)
+
+export default App;
--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App/App.jsx';
 
-class App extends React.Component {
-  render () {
-    return <p>Hello World!</p>;
-  }
-}
+render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('app'));
 
-render(<App/>, document.getElementById('app'));
